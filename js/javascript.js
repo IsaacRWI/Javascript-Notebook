a18246fe56529776d65d50072a5ae41602f05078
IsaacNgRWI <isaacngrwi@gmail.com>
2025-09-04 14:02:00 +0800

    Added if else statements and javascript f strings - Added if else statements to javascript.js - Added javascript equivalent of python f strings with back ticks ``

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,12 @@ var hello = "hello world";
 function compare (x, y) {
     return x > y;
 }
+
+var x;
+x = 5;
+if (x == undefined) {
+    console.log("x is undefined");
+}
+else {
+    console.log(`x is defined ${x}`);
+}
