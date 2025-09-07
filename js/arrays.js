723f21e028afc91c3f828357527976fe1f4f8766
IsaacRWI <isaacngrwi@gmail.com>
2025-09-07 19:03:10 +0100

    Added for loop calling undefined elements in an array

--- js/arrays.js
+++ js/arrays.js
@@ -10,5 +10,10 @@ console.log(array[0]);
 array[1](array[0]);
 
 // short hand array creation
-short_array = [1, 2, 3];
+short_array = ["monke", "pickle", "snake"];
 console.log(short_array);
+short_array[100] = "100th name"
+
+for (var i = 0; i < short_array.length; i++) {
+    console.log ("hello", short_array[i]);
+}
