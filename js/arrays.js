516b63665924168569e8273f910815d63d8b6a39
IsaacRWI <isaacngrwi@gmail.com>
2025-09-07 19:14:11 +0100

    Added for properties in object loop - Added possible problem when for prop in obj when used in arrays

--- js/arrays.js
+++ js/arrays.js
@@ -17,3 +17,19 @@ short_array[100] = "100th name"
 for (var i = 0; i < short_array.length; i++) {
     console.log ("hello", short_array[i]);
 }
+
+// object property retrieval
+var newobject = {
+    name: "monke",
+    age : 2,
+    food : "cheese"
+};
+
+for (var j in newobject) {
+    console.log(`${j}: ${newobject[j]} `);
+};
+
+short_array.hello = "hello"
+for (var i in short_array) {  // loops over all properties in objects, including things we dont want
+    console.log(short_array[i])
+};
