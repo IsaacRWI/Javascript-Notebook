506c9f56d559a9af5bb46dc41921354ef329dca4
IsaacRWI <isaacngrwi@gmail.com>
2025-09-07 18:58:17 +0100

    Added referencing array elements - Added array short hand creation

--- js/arrays.js
+++ js/arrays.js
@@ -6,3 +6,9 @@ array[1] = function (name) {
 array[2] = {key: "value"};
 
 console.log(array);
+console.log(array[0]);
+array[1](array[0]);
+
+// short hand array creation
+short_array = [1, 2, 3];
+console.log(short_array);
