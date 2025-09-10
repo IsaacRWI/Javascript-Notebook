b3270d6cc55cde75029732fb590fdb31e873d25d
IsaacRWI <isaacngrwi@gmail.com>
2025-09-10 22:09:49 +0100

    Added fake_namespaces.js to explain method to avoid namepsace overwritting - Added fake_namespaces.js - Added alternative to declaring common variables that can be overwritten - Added explanations to methods

---
+++ js/fake_namespaces.js
@@ -0,0 +1,12 @@
+// instead of
+var firstname = "name";  // var name can be overwritten in scripts called afterwards
+function hello () {
+    console.log("hello", firstname);  
+};
+
+// use
+var greeter = {};  // declares the thing as an object
+greeter.name = "name";  // variables unique to the object
+greeter.hello = function () {  // functions unique to the object
+    console.log("hello", greeter.name);
+};
